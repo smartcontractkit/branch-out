6ec634d3016534935778615cc82f3c64d17b09aa
Adam Hamrick <adam.hamrick@smartcontract.com>
2025-08-13 14:57:41 -0400

    Formatting

--- quarantine/quarantine.go
+++ quarantine/quarantine.go
@@ -7,6 +7,9 @@ import (
 	"testing"
 )
 
+// RunFlakyTestsEnvVar is the environment variable that controls whether to run flaky tests.
+const RunFlakyTestsEnvVar = "RUN_FLAKY_TESTS"
+
 // Flaky marks a test as flaky.
 // To run tests marked as flaky, set the RUN_FLAKY_TESTS environment variable to true.
 // To skip tests marked as flaky, set the RUN_FLAKY_TESTS environment variable to false (or don't set it at all).
@@ -26,15 +29,20 @@ func Flaky(tb testing.TB, ticket string) {
 	// Add this when we can use Go 1.25.0.
 	// tb.Attr("flaky", "true")
 	//nolint:forbidigo // Config doesn't make sense here
-	if os.Getenv("RUN_FLAKY_TESTS") != "true" {
+	if os.Getenv(RunFlakyTestsEnvVar) != "true" {
 		tb.Skipf(
-			"Skipping %s. To run flaky tests, set the RUN_FLAKY_TESTS environment variable to true.",
+			"Skipping %s. To run flaky tests, set the %s environment variable to true.",
 			explanationStr,
+			RunFlakyTestsEnvVar,
 		)
 	} else {
 		tb.Logf("Running %s", explanationStr)
 		tb.Cleanup(func() {
-			tb.Logf("Test is marked as flaky, but still ran. %s. To skip flaky tests, ensure the RUN_FLAKY_TESTS environment variable is set to false.", explanationStr)
+			tb.Logf(
+				"Test is marked as flaky, but still ran. %s. To skip flaky tests, ensure the %s environment variable is set to false.",
+				explanationStr,
+				RunFlakyTestsEnvVar,
+			)
 		})
 	}
 }
