ca1bc94cce8464742d9ea3123ba278b4b0e5b517
Adam Hamrick <adam.hamrick@smartcontract.com>
2025-08-13 15:57:56 -0400

    Better test'

--- quarantine/quarantine_test.go
+++ quarantine/quarantine_test.go
@@ -12,16 +12,19 @@ func TestFlaky(t *testing.T) {
 	t.Run("skip flaky tests", func(t *testing.T) {
 		t.Setenv("RUN_FLAKY_TESTS", "false")
 		quarantine.Flaky(t, "TEST-123")
-		didSkip := t.Skipped()
 
-		require.True(t, didSkip, "quarantined test should be skipped when RUN_FLAKY_TESTS is false")
+		t.Cleanup(func() {
+			require.True(t, t.Skipped(), "quarantined test should be skipped when RUN_FLAKY_TESTS is false")
+		})
 	})
 
 	t.Run("run flaky tests", func(t *testing.T) {
 		t.Setenv("RUN_FLAKY_TESTS", "true")
 		quarantine.Flaky(t, "TEST-123")
 
-		didSkip := t.Skipped()
-		require.False(t, didSkip, "quarantined test should not be skipped when RUN_FLAKY_TESTS is true")
+		t.Cleanup(func() {
+			require.False(t, t.Skipped(), "quarantined test should not be skipped when RUN_FLAKY_TESTS is true")
+			t.Log("This test is intentionally skipped! Skipping = Passing!")
+		})
 	})
 }
